6bb293b98886433536dcaf872b03cab95a20060a
ksoichiro <soichiro.kashima@gmail.com>
2014-08-09 13:22:38 +0900

    Separated file functions.

---
+++ file.go
@@ -0,0 +1,19 @@
+package main
+
+import "os"
+
+// Overwrite and create new file
+func createFile(filename string) (f *os.File) {
+	if fileExists(filename) {
+		os.Remove(filename)
+	}
+	f, _ = os.OpenFile(filename, os.O_RDWR|os.O_CREATE, 0666)
+	return
+}
+
+func fileExists(filename string) bool {
+	if _, err := os.Stat(filename); err != nil && os.IsNotExist(err) {
+		return false
+	}
+	return true
+}
--- mocker.go
+++ mocker.go
@@ -512,19 +512,3 @@ func genAndroidStyles(mock *Mock, valuesDir string) {
 
 	f.Close()
 }
-
-// Overwrite and create new file
-func createFile(filename string) (f *os.File) {
-	if fileExists(filename) {
-		os.Remove(filename)
-	}
-	f, _ = os.OpenFile(filename, os.O_RDWR|os.O_CREATE, 0666)
-	return
-}
-
-func fileExists(filename string) bool {
-	if _, err := os.Stat(filename); err != nil && os.IsNotExist(err) {
-		return false
-	}
-	return true
-}
